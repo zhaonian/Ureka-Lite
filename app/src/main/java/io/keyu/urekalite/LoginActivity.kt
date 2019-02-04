e19a06d37fcf442fc4d152303469bb936f2d7771
Zhaonian Luan <lzn0830@gmail.com>
2019-02-03 17:54:28 -0800

    feat: added redirection to Home from Login

--- app/src/main/java/io/keyu/urekalite/LoginActivity.kt
+++ app/src/main/java/io/keyu/urekalite/LoginActivity.kt
@@ -3,6 +3,7 @@ package io.keyu.urekalite
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.loginBtn
 import kotlinx.android.synthetic.main.activity_login.signupLink
 
 class LoginActivity : AppCompatActivity() {
@@ -10,6 +11,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            this.finish()
+        }
 
         signupLink.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
