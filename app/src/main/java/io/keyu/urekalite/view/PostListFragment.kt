68fa60a9d6dd3617cb5fc2b999eae11fd30d6f49
Zhaonian Luan <lzn0830@gmail.com>
2019-02-11 23:51:58 -0800

    feat: updated to MVVM

--- app/src/main/java/io/keyu/urekalite/view/PostListFragment.kt
+++ app/src/main/java/io/keyu/urekalite/view/PostListFragment.kt
@@ -5,30 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import io.keyu.urekalite.adapter.PostRecyclerViewAdapter
 import io.keyu.urekalite.R
+import io.keyu.urekalite.adapter.PostRecyclerViewAdapter
 import io.keyu.urekalite.model.Post
-import io.keyu.urekalite.service.PostDataService
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.DisposableObserver
-import io.reactivex.schedulers.Schedulers
+import io.keyu.urekalite.viewmodel.PostListViewModel
 
 class PostListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeLayout: SwipeRefreshLayout
-    private val postList: MutableList<Post> = ArrayList()
-    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private lateinit var postListViewModel: PostListViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView = inflater.inflate(R.layout.view_post_list, container, false)
-        getPostList()
         recyclerView = rootView.findViewById(R.id.postRecyclerView)
+        postListViewModel = ViewModelProviders.of(this).get(PostListViewModel::class.java)
+        getPostList()
 
         swipeLayout = rootView.findViewById(R.id.swipeLayout)
         swipeLayout.setColorSchemeResources(R.color.colorPrimary)
@@ -41,44 +38,28 @@ class PostListFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
-        compositeDisposable.clear() // to avoid memory leak
+        postListViewModel.clear() // to avoid memory leak
     }
 
     private fun getPostList() {
-        val retrofitInstance = PostDataService.retrofit
-        val postListObservable: Observable<List<Post>> = retrofitInstance.getPosts()
-        compositeDisposable.add(
-            postListObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .flatMapIterable { it }
-                .subscribeWith(object : DisposableObserver<Post>() {
-                    override fun onError(e: Throwable) {
-                        // if some error happens in our data layer our app will not crash, we will
-                        // get error here
-                    }
+        postListViewModel.getPostList().observe(this, Observer<List<Post>> {
+            data -> renderRecyclerView(data)
+        })
+    }
 
-                    override fun onNext(post: Post) {
-                        postList.add(post)
-                    }
+    private fun renderRecyclerView(data: List<Post>) {
+        recyclerView.apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
 
-                    override fun onComplete() {
-                        recyclerView.apply {
-                            // use this setting to improve performance if you know that changes
-                            // in content do not change the layout size of the RecyclerView
-                            setHasFixedSize(true)
+            // space between items
+            addItemDecoration(VerticalSpaceItemDecoration(36))
 
-                            // space between items
-                            addItemDecoration(VerticalSpaceItemDecoration(36))
+            // use a linear layout manager
+            layoutManager = LinearLayoutManager(context)
 
-                            // use a linear layout manager
-                            layoutManager = LinearLayoutManager(context)
-
-                            val viewAdapter = PostRecyclerViewAdapter(postList)
-                            recyclerView.adapter = viewAdapter
-                        }
-                    }
-                })
-        )
+            recyclerView.adapter = PostRecyclerViewAdapter(data)
+        }
     }
 }
