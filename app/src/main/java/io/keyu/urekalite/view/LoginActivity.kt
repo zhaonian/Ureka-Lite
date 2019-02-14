853987a9569c30b7f63e8f4e749fdfff27a65393
Zhaonian Luan <lzn0830@gmail.com>
2019-02-13 19:54:53 -0800

    feat: added validation

--- app/src/main/java/io/keyu/urekalite/view/LoginActivity.kt
+++ app/src/main/java/io/keyu/urekalite/view/LoginActivity.kt
@@ -10,7 +10,9 @@ import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_login.loginBtn
 import kotlinx.android.synthetic.main.activity_login.signupLink
 import kotlinx.android.synthetic.main.activity_login.emailTextView
+import kotlinx.android.synthetic.main.activity_login.emailTextLayout
 import kotlinx.android.synthetic.main.activity_login.passwordTextView
+import kotlinx.android.synthetic.main.activity_login.passwordTextLayout
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,16 +23,52 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         compositeDisposable.addAll(
-            RxTextView.textChanges(emailTextView).subscribe(),
-            RxTextView.textChanges(passwordTextView).subscribe(),
+            RxTextView.textChanges(emailTextView)
+                .map(CharSequence::toString)
+                .distinctUntilChanged()
+                .subscribe { input -> validateEmail(input) },
+
+            RxTextView.textChanges(passwordTextView)
+                .map(CharSequence::toString)
+                .distinctUntilChanged()
+                .subscribe { input -> validatePassword(input) },
+
             RxView.clicks(loginBtn).subscribe {
                 startActivity(Intent(this, HomeActivity::class.java))
                 this.finish()
             },
+
             RxView.clicks(signupLink).subscribe {
                 startActivity(Intent(this, SignupActivity::class.java))
                 this.finish()
             }
         )
+    }
+
+    private fun validateEmail(email: String) {
+        if (email.isNullOrBlank() || android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailTextLayout.isErrorEnabled = false
+        } else {
+            emailTextLayout.apply {
+                isErrorEnabled = true
+                error = "email does not look right 😰"
+            }
+        }
+    }
+
+    private fun validatePassword(password: String) {
+        if (password.isNullOrBlank() || (password.length in 6..32)) {
+            passwordTextLayout.isErrorEnabled = false
+        } else {
+            passwordTextLayout.apply {
+                isErrorEnabled = true
+                error = "password does not look right 😰"
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
