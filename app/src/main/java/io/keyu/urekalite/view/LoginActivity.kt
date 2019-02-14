63c18230835519a8f9a2e9179d0d4a5561c9a8c9
Zhaonian Luan <lzn0830@gmail.com>
2019-02-13 18:35:25 -0800

    feat: added RxBinding

--- app/src/main/java/io/keyu/urekalite/view/LoginActivity.kt
+++ app/src/main/java/io/keyu/urekalite/view/LoginActivity.kt
@@ -3,24 +3,34 @@ package io.keyu.urekalite.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxTextView
 import io.keyu.urekalite.R
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_login.loginBtn
 import kotlinx.android.synthetic.main.activity_login.signupLink
+import kotlinx.android.synthetic.main.activity_login.emailTextView
+import kotlinx.android.synthetic.main.activity_login.passwordTextView
 
 class LoginActivity : AppCompatActivity() {
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loginBtn.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
-            this.finish()
-        }
-
-        signupLink.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
-            this.finish()
-        }
+        compositeDisposable.addAll(
+            RxTextView.textChanges(emailTextView).subscribe(),
+            RxTextView.textChanges(passwordTextView).subscribe(),
+            RxView.clicks(loginBtn).subscribe {
+                startActivity(Intent(this, HomeActivity::class.java))
+                this.finish()
+            },
+            RxView.clicks(signupLink).subscribe {
+                startActivity(Intent(this, SignupActivity::class.java))
+                this.finish()
+            }
+        )
     }
 }
