b8f5dde81e3847f8123685e6f212811d088f518f
Zhaonian Luan <lzn0830@gmail.com>
2019-02-25 17:32:23 -0800

    feat: anim works now

--- app/src/main/java/io/keyu/urekalite/adapter/BookmarkViewHolder.kt
+++ app/src/main/java/io/keyu/urekalite/adapter/BookmarkViewHolder.kt
@@ -9,20 +9,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.facebook.drawee.view.SimpleDraweeView
 import io.keyu.urekalite.R
 import android.app.AlertDialog
+import android.util.Log
+import android.view.MotionEvent
 
 class BookmarkViewHolder(private val context: Context, itemView: View) :
     RecyclerView.ViewHolder(itemView),
     View.OnClickListener,
-    View.OnLongClickListener {
+    View.OnLongClickListener,
+    View.OnTouchListener {
 
     val postTitle: TextView
     val postImage: SimpleDraweeView
     val postAuthor: TextView
     val postText: TextView
 
+    private lateinit var alertDialog: AlertDialog
+    private var isLongPressed = false
+
     init {
         itemView.setOnClickListener(this)
         itemView.setOnLongClickListener(this)
+        itemView.setOnTouchListener(this)
 
         postTitle = itemView.findViewById(R.id.postTitle)
         postTitle.setTypeface(null, Typeface.BOLD)
@@ -36,6 +43,8 @@ class BookmarkViewHolder(private val context: Context, itemView: View) :
     }
 
     override fun onClick(view: View) {
+        Log.d("short", "cao!")
+
         Toast.makeText(
             view.context,
             "Clicked Position = $layoutPosition", Toast.LENGTH_SHORT
@@ -43,13 +52,24 @@ class BookmarkViewHolder(private val context: Context, itemView: View) :
     }
 
     override fun onLongClick(view: View): Boolean {
-
-        val alertDialog = AlertDialog.Builder(context).create()
+        Log.d("long", "cao!")
+        isLongPressed = true
+        alertDialog = AlertDialog.Builder(context).create()
         alertDialog.setTitle("Reset...")
         alertDialog.setMessage("R u sure?")
         alertDialog.window?.attributes?.windowAnimations = R.style.PostPreviewAnimation
         alertDialog.show()
 
+        return true
+    }
+
+    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+        view?.onTouchEvent(event)
+        if (event?.action == MotionEvent.ACTION_UP && isLongPressed) {
+            Log.d("touch", "cao!")
+            isLongPressed = false
+            alertDialog.dismiss()
+        }
         return true
     }
 }
