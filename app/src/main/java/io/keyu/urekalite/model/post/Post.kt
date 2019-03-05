f1ccf208d7469a187a88f339fb78a4f22fb97c28
Zhaonian Luan <lzn0830@gmail.com>
2019-03-04 16:38:53 -0800

    feat: added experiemental = true to get experimental Parcelize annotation

--- app/src/main/java/io/keyu/urekalite/model/post/Post.kt
+++ app/src/main/java/io/keyu/urekalite/model/post/Post.kt
@@ -1,12 +1,16 @@
 package io.keyu.urekalite.model.post
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
+@Parcelize
 data class Post(
-    @field:Json(name = "post") val content: Content,
-    @field:Json(name = "actionCount") val actionCount: ActionCount,
-    @field:Json(name = "tags") val tags: List<Tag>,
+    @field:Json(name = "post") val content: @RawValue Content,
+    @field:Json(name = "actionCount") val actionCount: @RawValue ActionCount,
+    @field:Json(name = "tags") val tags: @RawValue List<Tag>,
     @field:Json(name = "liked") val liked: Boolean,
     @field:Json(name = "bookmarked") val bookmarked: Boolean,
     @field:Json(name = "commented") val commented: Boolean
-)
+) : Parcelable
