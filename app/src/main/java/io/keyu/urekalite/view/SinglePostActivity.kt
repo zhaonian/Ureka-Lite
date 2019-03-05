ac88b957aeac6e094e970720b87719b7b243b7d5
Zhaonian Luan <lzn0830@gmail.com>
2019-03-05 15:29:51 -0800

    feat: added bottom toolbar for single post view

--- app/src/main/java/io/keyu/urekalite/view/SinglePostActivity.kt
+++ app/src/main/java/io/keyu/urekalite/view/SinglePostActivity.kt
@@ -1,12 +1,29 @@
 package io.keyu.urekalite.view
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.keyu.urekalite.R
+import kotlinx.android.synthetic.main.view_single_post.*
 
 class SinglePostActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_single_post)
+
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        like.setOnClickListener {
+            Toast.makeText(this, "like", Toast.LENGTH_LONG).show()
+        }
+        bookmark.setOnClickListener {
+            Toast.makeText(this, "bm", Toast.LENGTH_LONG).show()
+        }
+        link.setOnClickListener {
+            Toast.makeText(this, "link", Toast.LENGTH_LONG).show()
+        }
     }
 }
