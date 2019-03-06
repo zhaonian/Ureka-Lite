bf573e3611ed8cc0ba1e9adb035412203710357c
Zhaonian Luan <lzn0830@gmail.com>
2019-03-05 19:36:01 -0800

    feat: added awesome cool Medium-like full-screen mode

--- app/src/main/java/io/keyu/urekalite/view/SinglePostActivity.kt
+++ app/src/main/java/io/keyu/urekalite/view/SinglePostActivity.kt
@@ -3,8 +3,10 @@ package io.keyu.urekalite.view
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import io.keyu.urekalite.R
 import kotlinx.android.synthetic.main.view_single_post.*
+import android.view.View
 
 class SinglePostActivity : AppCompatActivity() {
 
@@ -27,6 +29,22 @@ class SinglePostActivity : AppCompatActivity() {
         }
         share.setOnClickListener {
             Toast.makeText(this, "share", Toast.LENGTH_LONG).show()
+        }
+
+        nestedScrollView.setOnScrollChangeListener { v: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int ->
+            // Scroll Down
+            if (scrollY > oldScrollY) {
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE)
+            }
+            // Scroll Up
+            else if (scrollY < oldScrollY) {
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            }
         }
     }
 }
