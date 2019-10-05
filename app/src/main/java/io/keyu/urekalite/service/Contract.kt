4f61977d97a39415fd2c73ca472a22a4a1844a89
Zhaonian Luan <lzn0830@gmail.com>
2019-10-04 23:41:42 -0700

    hide AWS API key

--- app/src/main/java/io/keyu/urekalite/service/Contract.kt
+++ app/src/main/java/io/keyu/urekalite/service/Contract.kt
@@ -1,5 +1,7 @@
 package io.keyu.urekalite.service
 
+import io.keyu.urekalite.BuildConfig
+
 object Contract {
-    const val UREKA_AWS = "http://ec2-52-15-224-200.us-east-2.compute.amazonaws.com:8080/api"
+    const val UREKA_AWS = BuildConfig.ApiKey
 }
